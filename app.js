b0d91e66c4150451a05676b9baed8c072e3d84a2
cinecus <chananon.seen@gmail.com>
2021-08-12 15:56:22 +0700

    deploy to heroku v1

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+//security
+const cors = require('cors')
 
+//db connect
+const connectDB = require('./db/connect');
 
 //routers
-const indexRouter = require('./routes/index')
+const studentRouter = require('./routes/student')
 
 //error handler
 const notFoundMiddleware = require('./middleware/not-found')
@@ -15,22 +19,25 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 
 app.use(express.json())
+//security
+app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Backend Project Template</h1><a href="/api/v1/index">Go !!</a>"')
+    res.send('<h1>Backend Project Template</h1><a href="/api/v1/student">Go !!</a>"')
 })
 
 //routes
-app.use('/api/v1/index',indexRouter)
+app.use('/api/v1/student',studentRouter)
 
 //middleware
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const start = async() =>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`Server is listening on port ${port}`))
     } catch (error) {
         console.log(error)
